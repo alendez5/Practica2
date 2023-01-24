7607853e34cf04d9b225f5289ee7a21f62ab1f69
Sergiokwsk <sergio.kossowski@tiernoparla.es>
2023-01-24 19:52:05 +0100

    añado la clase ObraPictorica, implementando el toString

---
+++ src/Obras/ObraPictorica.java
@@ -0,0 +1,36 @@
+package Obras;
+
+
+public class ObraPictorica extends ObraDeArtes{
+	
+	private String tecnica;
+	
+	public ObraPictorica(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+			int piezas, String descripcion,String tecnica) {
+		
+		super(id, nombre, autor, tipo, precio, altura, peso, piezas, descripcion);
+		
+		this.tecnica = tecnica;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("Nombre : " + getNombre() + "\n");
+		list.append("Autor : " + getAutor() + "\n");
+		list.append("Precio : " + getPrecio() + "\n");
+		list.append("Altura : " + getAltura() + "\n");	
+		list.append("Peso : " + getNombre() + "\n");
+		list.append("Numero de piezas : " + getPiezas() + "\n");
+		list.append("Descripcion : " + getDescripcion() + "\n");
+		list.append("Tecnica : " + tecnica + "\n");
+		
+		return list.toString();
+		
+	}
+	
+	
+
+}
