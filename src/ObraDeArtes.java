dcb9296a54dbd741c3928516623039ff315e2aa1
Sergiokwsk <sergio.kossowski@tiernoparla.es>
2023-01-24 19:05:47 +0100

    añado la clase abstracta ObraDeArtes

---
+++ src/ObraDeArtes.java
@@ -0,0 +1,36 @@
+
+public abstract class ObraDeArtes {
+	
+	private int id;
+	private String nombre;
+	private String autor;
+	private String tipo;
+	private double precio;
+	private double altura;
+	private double peso;
+	private double piezas;
+	private String descripcion;
+	
+	public ObraDeArtes(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+			double piezas, String descripcion) {
+		
+		this.id = id;
+		this.nombre = nombre;
+		this.autor = autor;
+		this.tipo = tipo;
+		this.precio = precio;
+		this.altura = altura;
+		this.peso = peso;
+		this.piezas = piezas;
+		this.descripcion = descripcion;
+	}
+
+	
+
+	@Override
+	public abstract String toString();
+	
+
+	
+
+}
