60c71a4ebf0e0948947ad99269bc062c6c2514df
Sergiokwsk <sergio.kossowski@tiernoparla.es>
2023-01-24 20:00:31 +0100

    Añado la clase ObraEstructura, implementando el toString

---
+++ src/Obras/ObraEstructura.java
@@ -0,0 +1,33 @@
+package Obras;
+
+public class ObraEstructura extends ObraDeArtes{
+	
+	private String material;
+
+	public ObraEstructura(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+			int piezas, String descripcion, String material) {
+		
+		super(id, nombre, autor, tipo, precio, altura, peso, piezas, descripcion);
+		
+		this.material = material;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder list = new StringBuilder();
+		list.append("Nombre : " + getNombre() + "\n");
+		list.append("Autor : " + getAutor() + "\n");
+		list.append("Precio : " + getPrecio() + "\n");
+		list.append("Altura : " + getAltura() + "\n");	
+		list.append("Peso : " + getNombre() + "\n");
+		list.append("Numero de piezas : " + getPiezas() + "\n");
+		list.append("Descripcion : " + getDescripcion() + "\n");
+		list.append("Material : " + material + "\n");
+		
+		return list.toString();
+		
+	}
+
+}
