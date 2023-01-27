6cf263029fb989deeea751adfbd9d7ddf4c69823
Sergiokwsk <sergio.kossowski@tiernoparla.es>
2023-01-27 20:37:04 +0100

    añado getter y setter

--- src/Obras/ObraEscultura.java
+++ src/Obras/ObraEscultura.java
@@ -30,4 +30,14 @@ public class ObraEscultura extends ObraDeArtes{
 		
 	}
 
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	
+	
+
 }
